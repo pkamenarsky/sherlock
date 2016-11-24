ac33d412872e4fd8ccd8b680cf01cbbf1c0b9eb2
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-11-25 01:02:25 +0200

    Horizontal delimiters

--- src/script.js
+++ src/script.js
@@ -6,43 +6,48 @@ function render(log) {
     var item = document.createElement("div");
     item.className = 'item';
 
-    if (msg.log_data.log_level) {
-      item.className = 'item ' + msg.log_data.log_level;
+    if (msg.log_delimiter) {
+      item.className = 'delimiter';
     }
+    else {
+      if (msg.log_data.log_level) {
+        item.className = 'item ' + msg.log_data.log_level;
+      }
 
-    if (msg.log_data.log_color) {
-      item.style.color = msg.log_data.log_color;
-    }
+      if (msg.log_data.log_color) {
+        item.style.color = msg.log_data.log_color;
+      }
 
-    if (msg.log_data.log_type) {
-        var kv = document.createElement("div");
-        kv.className = 'kv log-type';
-        item.appendChild(kv);
+      if (msg.log_data.log_type) {
+          var kv = document.createElement("div");
+          kv.className = 'kv log-type';
+          item.appendChild(kv);
 
-        var v = document.createElement("div");
-        v.className = 'v';
-        v.textContent = msg.log_data.log_type;
-        kv.appendChild(v);
-    }
+          var v = document.createElement("div");
+          v.className = 'v';
+          v.textContent = msg.log_data.log_type;
+          kv.appendChild(v);
+      }
 
-    for (var key in msg.log_data) {
-      if (key !== 'log_type'
-          && key !== 'log_level'
-          && key !== 'log_color'
-          && msg.log_data.hasOwnProperty(key)) {
-        var kv = document.createElement("div");
-        kv.className = 'kv';
-        item.appendChild(kv);
+      for (var key in msg.log_data) {
+        if (key !== 'log_type'
+            && key !== 'log_level'
+            && key !== 'log_color'
+            && msg.log_data.hasOwnProperty(key)) {
+          var kv = document.createElement("div");
+          kv.className = 'kv';
+          item.appendChild(kv);
 
-        var k = document.createElement("div");
-        k.className = 'k';
-        k.textContent = key;
-        kv.appendChild(k);
+          var k = document.createElement("div");
+          k.className = 'k';
+          k.textContent = key;
+          kv.appendChild(k);
 
-        var v = document.createElement("div");
-        v.className = 'v';
-        v.textContent = msg.log_data[key];
-        kv.appendChild(v);
+          var v = document.createElement("div");
+          v.className = 'v';
+          v.textContent = msg.log_data[key];
+          kv.appendChild(v);
+        }
       }
     }
 
@@ -70,4 +75,15 @@ function main() {
 
   s.onerror = function(msg) {
   };
+
+  window.onkeyup = function(e) {
+     var key = e.keyCode ? e.keyCode : e.which;
+
+     if (key == 72) {
+       if (!log[0] || (log[0] && !log[0].log_delimiter)) {
+         log.unshift({log_id: id++, log_delimiter: true});
+         render(log);
+       }
+     }
+  }
 }
